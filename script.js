8ded3a10bd7508c7d487b78c589bd5cd1a7c4164
jgoceljak <john.goceljak@gmail.com>
2023-07-18 18:34:26 -0400

    Selecting pixels working

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+let selectedColor = "black"
 
 
 function createBoard(size) {
@@ -13,17 +13,34 @@ function createBoard(size) {
 
         for (let j = 0; j < size; j++){
            let square = document.createElement("div");
-           console.log("run")
            square.classList.add("pixel");
-           square.setAttribute('id', pixelNumber)
-           row.appendChild(square)
-           pixelNumber++
+           square.setAttribute('id', "p" + pixelNumber);
+           row.appendChild(square);
+           pixelNumber++;
         }
         container.appendChild(row)
     }
 }
 
+function colorPixel(pixel,selectedColor) {
+    let id = String(pixel)
+    let color = selectedColor
+    let currentPixel = document.querySelector("#" + id)
+    currentPixel.setAttribute('style', 'background-color: ' + color + ';')
+    return
+}
 
 createBoard(16);
+
+const board = document.querySelector(".container")
+board.addEventListener('click', (event) => {
+    const isButton = event.target.nodeName === 'BUTTON';
+
+    let pixelID = event.target.id
+    console.log(pixelID)
+    colorPixel(pixelID, selectedColor)
+
+})
+
 
 
